86fbd44029f819a668ca5b2b408bb13d99c0040b
czhao <czhao1@gmail.com>
2015-10-06 02:33:57 +0000

    changed get url to /nickname and response text to lively dolphin

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+app.get('/nickname', function(request, response){
+    response.send('lively dolphin');
 });
 
 app.listen(process.env.PORT || 4000);
